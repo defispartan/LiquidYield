041fa5898de3e3f0b30d90dabd3ee50217cbef45
Andrew Schmidt <schmi651@msu.edu>
2021-01-23 19:11:46 -0600

    Pushed query changes as well

--- src/components/Data/Query.js
+++ src/components/Data/Query.js
@@ -130,6 +130,16 @@ export const GET_DERIVED_ETH = gql`
   }
 `;
 
+export const GET_INDEX_PRICES = gql`
+  query tokens {
+    tokens(where:{id_in: ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"]}) {
+      id
+      name
+      derivedETH
+    }
+  }
+`;
+
 export const GET_REWARD_POOLS = gql`
   query pools {
     pools(where: { allocPoint_gt: 0 }) {
@@ -163,8 +173,8 @@ export const GET_POOL_INFO = gql`
 export const GET_UNI_DAY_DATA = gql`
   query dayData {
     uniswapDayDatas(
-      first: 100
-      where: { dailyVolumeUSD_gt: 0 }
+      first: 1000
+      where: { date_gt: 1589792400 }
       orderBy: date
       orderDirection: desc
     ) {
@@ -178,8 +188,8 @@ export const GET_UNI_DAY_DATA = gql`
 export const GET_SUSHI_DAY_DATA = gql`
   query dayData {
     dayDatas(
-      first: 100
-      where: { volumeUSD_gt: 0 }
+      first: 1000
+      where: { date_gt: 1589792400 }
       orderBy: date
       orderDirection: desc
     ) {
